b475b1a52e00346071a2ae519d464fc8208c8344
Cru3 <technomad@protonmail.com>
2016-05-16 14:11:32 -0400

    Update enumME.sh

--- enumME.sh
+++ enumME.sh
@@ -6,16 +6,16 @@ echo "Type an IPV4 address to enumerate."
 
 read IP
 
-mkdir Target$IP && nmap -sV -A -O --open -p1-65535 $IP > Target$IP/TargetServices.txt &
+mkdir Target$IP && nmap -sV -A -O --open -p1-65535 $IP > Target$IP/Services.txt &
 
 
 
 sleep 0.5
-nmap --script=vuln --script-args=unsafe=1 $IP > Target$IP/TargetVulnerability.txt &
-nmap --script=intrusive $IP > Target$IP/TargetBrute.txt &
-nmap --script=exploit $IP > Target$IP/TargetExploit.txt &
-dirb http://$IP > Target$IP/TargetDIRB.txt &
-nikto -h $IP > Target$IP/TargetNIKTO.txt &
-enum4linux $IP > Target$IP/TargetEnum4Linux.txt &
-nbtscan -v $IP > Target$IP/TargetNBTScan.txt; done
+nmap --script=vuln --script-args=unsafe=1 $IP > Target$IP/Vulnerability.txt &
+nmap --script=intrusive $IP > Target$IP/Intrusive.txt &
+nmap --script=exploit $IP > Target$IP/Exploit.txt &
+dirb http://$IP > Target$IP/DIRB.txt &
+nikto -h $IP > Target$IP/NIKTO.txt &
+enum4linux $IP > Target$IP/Enum4Linux.txt &
+nbtscan -v $IP > Target$IP/NBTScan.txt; done
 #msfconsole
