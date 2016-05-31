4ceee16ab392df1b1e69489956e280de6dfc9258
Cru3 <technomad@protonmail.com>
2016-05-31 10:30:13 -0400

    Changed for any distribution that can git clone
    Functional enumeration scripts. Changed for any distribution that has E4L & dirb - you may have to edit it yourself. This was a quick build to run through boxes in the OSCP as host to host to host manual scanning is very inefficient. This also allows you to categorize hosts and keep that information for later without the extra effort of command > stdout.txt for each and every scan. It's simple, but effective.

--- enumME.sh
+++ enumME.sh
@@ -1,37 +1,40 @@
+
 #!/bin/bash
 
-####################################################____KALI_LINUX_____###############################################################
+######################################################RAPID#ENUMERATION#SCRIPT####################################################
 
 
 #proxychains
-echo "READ ME: Runs basic enumeration and vulnerability scripts in the background and generates a unique directory for that target."
-sleep 1.5s
-tput clear
+echo "READ ME: Runs basic enumeration and vulnerability scripts and commits them to a custom directory.
+sleep 0.2s
 
 echo "Please type an IPV4 address to enumerate and press enter."
 
 read IP
 
-mkdir $IP && nmap -sV -A -O --open -p1-65535 $IP > $IP/Services.txt &
+mkdir $IP && nmap --open -p1-65535 $IP > $IP/Open.txt &
 
 echo "Beginning scans on your target of" $IP "Standby." &
 sleep 1.0s
 
 
 echo "Quick port scan to look around while other scans finish. Please check corresponding directory."
-nmap --min-rate 10000 -p 21,22,23,80,137,139,443,3306,3309 $IP > $IP/Quickscan.txt
+nmap --min-rate 10000 -p 21,22,23,80,137,139,443,445,3306,3309,3389 $IP > $IP/Quick.txt
 sleep 1s
-cat $IP/Quickscan.txt | grep open
+cat $IP/Quick.txt | grep open
 
 
 
 sleep 2s
-nmap --script=vuln --script-args=unsafe=1 $IP > $IP/Vulns.txt &
-nmap --script=intrusive $IP > $IP/Intrusive.txt &
-nmap --script=exploit $IP > $IP/Exploit.txt &
-dirb http://$IP > $IP/DIRB.txt &
-nikto -h $IP > $IP/NIKTO.txt &
-enum4linux $IP > $IP/Enum4Linux.txt &
-nbtscan -v $IP > $IP/NBTScan.txt &
-rm -rf $IP/Quickscan.txt; done
+
+./dirb/dirb http://$IP > $IP/DIRB.txt &
+nbtscan $IP > $IP/NBTScan.txt &
+enum4linux/enum4linux.pl $IP > $IP/Enum4Linux.txt &
+nmap -sV -sC -v $IP > $IP/Services.txt &&
+nmap --script=vuln --script-args=unsafe=1 -vv $IP > $IP/Vulns.txt &&
+nmap --script=exploit --script-args=unsafe=1 $IP -vv > $IP/Exploit.txt &&
+nmap --script=intrusive $IP -vv > $IP/Intrusive.txt &&
+
+rm -rf $IP/Quick.txt; done
 #msfconsole
+
